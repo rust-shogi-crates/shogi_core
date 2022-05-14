5b29aa82df08058dee314ec2278af351e3b8d639
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2022-05-15 03:20:33 +0900

    Enhance documents in to_usi.rs

--- shogi_core/src/to_usi.rs
+++ shogi_core/src/to_usi.rs
@@ -3,13 +3,18 @@ use core::fmt::{Result as FmtResult, Write};
 /// A type that is convertible to USI format.
 pub trait ToUsi {
     /// Write `self` in USI format.
+    ///
+    /// This function returns Err(core::fmt::Error)
+    /// if and only if it fails to write to `sink`.
     fn to_usi<W: Write>(&self, sink: &mut W) -> FmtResult;
 
     /// Returns `self`'s string representation.
     #[cfg(feature = "alloc")]
     fn to_usi_owned(&self) -> alloc::string::String {
         let mut s = alloc::string::String::new();
-        self.to_usi(&mut s).unwrap();
+        // guaranteed to be Ok(())
+        let result = self.to_usi(&mut s);
+        debug_assert_eq!(result, Ok(()));
         s
     }
 }
